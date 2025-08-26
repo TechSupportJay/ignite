c5afe75f599cfbf2e0c695e7a91d0943eb4c8f6d
Jay <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>
2025-08-26 12:18:13 +0100

    oops forgot to update the version string

--- Assets/Scenes/main_menu.py
+++ Assets/Scenes/main_menu.py
@@ -132,7 +132,7 @@ def init(data = []):
     camera.add_item(background)
 
     ### Version String
-    version = RMS.objects.text("version", "Version 0.8.5")
+    version = RMS.objects.text("version", "Version 0.9.0")
     version.set_property("font", skin_grab("Fonts/default.ttf"))
     version.set_property("font_size", 20)
     version.set_property("text_align", "center")
